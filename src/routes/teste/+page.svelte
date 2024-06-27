79294f3e688ec1eb4cd542a11b332b459496a41e
Rodrigo Serra <xyrsto123@gmail.com>
2024-06-27 19:46:12 +0100

    Tailwind build test

---
+++ src/routes/teste/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+</script>
+
+<h1>Teste</h1>
+<button class="rounded bg-green-300 hover:bg-green-600">Tailwind test</button>
--- svelte.config.js
+++ svelte.config.js
@@ -13,10 +13,12 @@
 //export default config;
 
 import node from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	kit: {
 		adapter: node(),
-	}
+	},
+	preprocess: vitePreprocess()
 }
 
 export default config;
